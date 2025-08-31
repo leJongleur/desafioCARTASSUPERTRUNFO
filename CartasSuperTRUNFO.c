e9570ef9e5c6e58e96dce2c3a8f34bddc877e7c7
leJongleur <gomescarneiro@rocketmail.com>
2025-08-31 01:54:47 -0300

    Add files via upload

--- CartasSuperTRUNFO.c
+++ CartasSuperTRUNFO.c
@@ -1,1 +1,56 @@
+#include <stdio.h>
 
+int main() {
+    // Variáveis da carta 1
+    char codigo1[5];
+    int populacao1, pontos1;
+    float area1, pib1;
+
+    // Variáveis da carta 2
+    char codigo2[5];
+    int populacao2, pontos2;
+    float area2, pib2;
+
+    // Cadastro da carta 1
+    printf("Cadastro da Carta 1\n");
+    printf("Digite o codigo da carta (ex: A01): ");
+    scanf("%s", codigo1);
+    printf("Digite a populacao: ");
+    scanf("%d", &populacao1);
+    printf("Digite a area: ");
+    scanf("%f", &area1);
+    printf("Digite o PIB: ");
+    scanf("%f", &pib1);
+    printf("Digite o numero de pontos turisticos: ");
+    scanf("%d", &pontos1);
+
+    // Cadastro da carta 2
+    printf("\nCadastro da Carta 2\n");
+    printf("Digite o codigo da carta (ex: B02): ");
+    scanf("%s", codigo2);
+    printf("Digite a populacao: ");
+    scanf("%d", &populacao2);
+    printf("Digite a area: ");
+    scanf("%f", &area2);
+    printf("Digite o PIB: ");
+    scanf("%f", &pib2);
+    printf("Digite o numero de pontos turisticos: ");
+    scanf("%d", &pontos2);
+
+    // Exibindo os dados
+    printf("\n--- Dados da Carta 1 ---\n");
+    printf("Codigo: %s\n", codigo1);
+    printf("Populacao: %d\n", populacao1);
+    printf("Area: %.2f\n", area1);
+    printf("PIB: %.2f\n", pib1);
+    printf("Pontos turisticos: %d\n", pontos1);
+
+    printf("\n--- Dados da Carta 2 ---\n");
+    printf("Codigo: %s\n", codigo2);
+    printf("Populacao: %d\n", populacao2);
+    printf("Area: %.2f\n", area2);
+    printf("PIB: %.2f\n", pib2);
+    printf("Pontos turisticos: %d\n", pontos2);
+
+    return 0;
+}
